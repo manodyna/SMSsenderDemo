6bd399a499d92c25ce4aacc4c2e54e2254c0964e
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-05-29 17:11:24 +0530

    file sent

---
+++ src/main/java/com/pesuio/smssenderdemo/Controllers/SMScontroller2.java
@@ -0,0 +1,14 @@
+package com.pesuio.smssenderdemo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SMScontroller2 {
+
+    @GetMapping("/helloFromFile")
+    public String helloFromFile(){
+        return "hello";
+    }
+}
